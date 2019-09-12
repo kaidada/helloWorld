d3d20dc3cc0232e7659dbd53684c0a4beda680a5
'shiguanghua' <'shiguanghua@qianrh.com'>
2019-09-12 09:54:13 +0800

    test

--- src/com/xsk/testGit/HelloWorldGitTest.java
+++ src/com/xsk/testGit/HelloWorldGitTest.java
@@ -3,6 +3,6 @@ package com.xsk.testGit;
 public class HelloWorldGitTest {
 
 	public static void main(String args[]) { 
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World!gaigaishishi"); 
     } 
 }
